11aca2a46fe89cc04eaaa35ec4e5b8b6c55552b5
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-20 16:40:02 +0100

    corretto il controllo livello difficoltà

--- js/main.js
+++ js/main.js
@@ -22,59 +22,63 @@ function errorNumberDifficolta(livelloDifficolta){
   if (livelloDifficolta < 1 || livelloDifficolta > 2  || isNaN(livelloDifficolta)) {
     controllo = true;
   }
-  return controllo;
-}
-//estraggo 16 numeri random da 1 a 100
-var numeroCpu;
-var arrayNumeriCpu = [];
-for (var i = 1; i <= 16; i++) {
-  numeroCpu = numeroRandom(livelloDifficolta);
-  var erroreInserimentoDifficolta = errorNumberDifficolta(livelloDifficolta);
-
-  if(erroreInserimentoDifficolta) {
-    alert('Livello di difficoltà valida, inserisci numero da 0 a 2');
-  }
   else {
-    console.log("giro " + i + " numero random " + numeroCpu)
-    arrayNumeriCpu.push(numeroCpu);
-  }
-
-}
-console.log("Array numeri della cpu: " + arrayNumeriCpu);
-//chiedo all'utente di inserire un numero da 1 a 100,
-//uno alla volta
-var numeroUtente;
-//funzione gestione errore numeroUtente
-function errorNumber(numeroUtente){
-  var controllo = false;
-  if (numeroUtente < 1 || numeroUtente > 100  || isNaN(numeroUtente)) {
-    controllo = true;
+    alert('no');
   }
   return controllo;
 }
-var punto = 1;
-var score = 0;
-var contatore = 0;
-do {
-  numeroUtente = parseInt(prompt("inserisci un numero da 1 a 100"));
-  var erroreInserimento = errorNumber(numeroUtente);
-  console.log("stato errore: " + erroreInserimento);
-  //eseguo il controllo finale in caso di true
-  if (erroreInserimento) {
-    alert('Numero non valido, inserisci numero da 1 a 100!');
-  }
-  else {
-    // in caso di false vado avanti
-    console.log("giro " + i + " numero utente " + numeroUtente);
-    score += punto;
-    contatore ++;
-    console.log("valore contatore: " + contatore)
-  }
-} while
- (!arrayNumeriCpu.includes(numeroUtente) && contatore < 2);
+var erroreLivelloDifficolta = errorNumberDifficolta(livelloDifficolta);
+if (erroreLivelloDifficolta) {
+  alert('Inserisci il livello giusto, da 0 a 2!');
+}
+else {
+  //estraggo 16 numeri random da 1 a 100
+  var numeroCpu;
+  var arrayNumeriCpu = [];
+  var i = 0;
+  do {
+      numeroCpu = numeroRandom(livelloDifficolta);
+      console.log("giro " + i + " numero random " + numeroCpu)
+      arrayNumeriCpu.push(numeroCpu);
+      i++;
+  } while (i <= 16);
 
-alert("partita terminata");
-console.log("score finale : " + score);
+  console.log("Array numeri della cpu: " + arrayNumeriCpu);
+  //chiedo all'utente di inserire un numero da 1 a 100,
+  //uno alla volta
+  var numeroUtente;
+  //funzione gestione errore numeroUtente
+  function errorNumber(numeroUtente){
+    var controllo = false;
+    if (numeroUtente < 1 || numeroUtente > 100  || isNaN(numeroUtente)) {
+      controllo = true;
+    }
+    return controllo;
+  }
+  var punto = 1;
+  var score = 0;
+  var contatore = 0;
+  do {
+    numeroUtente = parseInt(prompt("inserisci un numero da 1 a 100"));
+    var erroreInserimento = errorNumber(numeroUtente);
+    console.log("stato errore: " + erroreInserimento);
+    //eseguo il controllo finale in caso di true
+    if (erroreInserimento) {
+      alert('Numero non valido, inserisci numero da 1 a 100!');
+    }
+    else {
+      // in caso di false vado avanti
+      console.log("giro " + i + " numero utente " + numeroUtente);
+      score += punto;
+      contatore ++;
+      console.log("valore contatore: " + contatore)
+    }
+  } while
+   (!arrayNumeriCpu.includes(numeroUtente) && contatore < 2);
+
+  alert("partita terminata");
+  console.log("score finale : " + score);
+}
 //Se inserisce un numero vietato la partita termina (numeri vietati 16)
 //quindi numero massimo è 100 - 16 = 84 numeri disponibili
 //Se si supera il numero massimo 84 di lanci la partita termina
