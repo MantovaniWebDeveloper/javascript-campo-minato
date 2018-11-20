4dd87817c03ffd63d1cc75f881498a6afc77b971
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-20 14:46:25 +0100

    realizzata prima bozza per i numeri utenti ed il confronto

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 // creo una funzione dove gli passo  il range da 1 a 100 random
-var numeroMin = 1;
-var numeroMax = 100;
-
 function numeroRandom(numeroMin, numeroMax){
   var numRandomRisultante = Math.floor(Math.random() * (numeroMax - numeroMin + 1) - numeroMin);
   return numRandomRisultante;
 }
-
 //estraggo 16 numeri random da 1 a 100
-var numeroCpu
+var numeroCpu;
 var arrayNumeriCpu = [];
-for (var i = 0; i < 16; i++) {
+for (var i = 1; i <= 16; i++) {
   numeroCpu = numeroRandom(1,100);
   console.log("giro " + i + " numero random " + numeroCpu)
   arrayNumeriCpu.push(numeroCpu);
@@ -18,12 +14,23 @@ for (var i = 0; i < 16; i++) {
 console.log("Array numeri della cpu: " + arrayNumeriCpu);
 //chiedo all'utente di inserire un numero da 1 a 100,
 //uno alla volta
-var arrayNumeriUtente = ["3","4","17","33",""];
+var numeroUtente;
+for (var i = 1; i <= 4; i++) {
+  numeroUtente = parseInt(prompt("inserisci un numero da 1 a 100"));
+  console.log("giro " + i + " numero utente " + numeroUtente);
+  //confronto i numeri inseriti fino a trovare se c'è il numero che combacia
+  //con quelli generati dalla cpu
+  if (arrayNumeriCpu[i] == numeroUtente || i > 2) {
+    //Se combacia la partita termina
+    console.log("Eisto partita: TERMINATA");
+    alert("terminata");
+  }
+  else {
+    console.log("Eisto partita: CONTINUA");
+  }
+}
 
-//confronto i numeri inseriti fino a trovare se c'è il numero che combacia
-//con quelli generati dalla cpu
 
-//Se combaci la partita termina
 
 //Se inserisce un numero vietato la partita termina (numeri vietati 16)
 //quindi numero massimo è 100 - 16 = 84 numeri disponibili
