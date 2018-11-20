45f3aa6d11c4df1ebf5e7eb13918c006499654ab
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-20 15:37:11 +0100

    inserito funzione gestione errore sui numeri da 1 a 100

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function numeroRandom(numeroMin, numeroMax){
   var numRandomRisultante = Math.floor(Math.random() * (numeroMax - numeroMin + 1) - numeroMin);
   return numRandomRisultante;
 }
+
 //estraggo 16 numeri random da 1 a 100
 var numeroCpu;
 var arrayNumeriCpu = [];
@@ -15,23 +16,29 @@ console.log("Array numeri della cpu: " + arrayNumeriCpu);
 //chiedo all'utente di inserire un numero da 1 a 100,
 //uno alla volta
 var numeroUtente;
+//funzione gestione errore numeroUtente
+function errorNumber(numeroUtente){
+  var controllo;
+  if (numeroUtente < 1 || numeroUtente > 100) {
+    controllo = "Numero non valido, inserisci numero da 1 a 100";
+  }
+  return controllo;
+}
 var punto = 1;
 var score = 0;
 var contatore = 0;
-
 do {
   numeroUtente = parseInt(prompt("inserisci un numero da 1 a 100"));
+  var erroreInserimento = errorNumber(numeroUtente);
+  alert(erroreInserimento);
   console.log("giro " + i + " numero utente " + numeroUtente);
   score += punto;
   contatore ++;
   console.log("valore contatore: " + contatore)
-
-} while (!arrayNumeriCpu.includes(numeroUtente) && contatore < 2);
-
+} while
+ (!arrayNumeriCpu.includes(numeroUtente) && contatore < 2);
+alert("partita terminata");
 console.log("score finale : " + score);
-
-
 //Se inserisce un numero vietato la partita termina (numeri vietati 16)
 //quindi numero massimo è 100 - 16 = 84 numeri disponibili
-
 //Se si supera il numero massimo 84 di lanci la partita termina
