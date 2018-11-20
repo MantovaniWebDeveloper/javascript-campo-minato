829e3123828b20ff7da907e4442e94f2d724f3d4
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-20 14:12:58 +0100

    init con logica in italiano scritto main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// creo una funzion dove gli passo  il rage da 1 a 100 random
+
+//estraggo 16 numeri random da 1 a 100
+
+//chiedo all'utente di inserire un numero da 1 a 100,
+//uno alla volta
+
+
+//confronto i numeri inseriti fino a trovare se ce il numero che combacia
+//con quelli generati dalla cpu
+
+//Se combaci la partita termina
+
+//Se inserisce un numero vietato la partita termina (numeri vietati 16)
+//quindi numero massimo è 100 - 16 = 84 numeri disponibili
+
+//Se si supera il numero massimo 84 di lanci la partita termina
